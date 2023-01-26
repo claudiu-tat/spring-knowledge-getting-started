1159bd715296114368e004e4d1fa08e76196aac0
Claudiu Tat <clawtat@gmail.com>
2023-01-27 00:50:52 +0200

    Springboot first project

---
+++ springboot-first-project/src/main/java/ro/sda/first_project/SpringBootFirsApp.java
@@ -0,0 +1,12 @@
+package ro.sda.first_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirsApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFirsApp.class, args);
+    }
+}
---
+++ springboot-first-project/src/main/java/ro/sda/first_project/controller/HelloController.java
@@ -0,0 +1,11 @@
+package ro.sda.first_project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping({"/hello"})
+    public String hello() { return "hello.html"; }
+}
